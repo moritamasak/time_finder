e4509b6c9dd8916fc918b542256acba98d00a443
masaki morita <m_masaki_0701@softbank.ne.jp>
2023-04-10 11:20:54 +0900

    devise導入の指示1設定のため

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :statuses
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
