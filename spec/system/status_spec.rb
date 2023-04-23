d47fb2d8ac795070b12b5c1b74203a0e7b4be2b6
masaki morita <m_masaki_0701@softbank.ne.jp>
2023-04-24 00:01:28 +0900

    海外メンターの指摘訂正のため（翻訳とview画面の訂正）

---
+++ spec/system/status_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe 'ステータス機能', type: :system do
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:second_user){ FactoryBot.create(:second_user)}
+  let!(:status) { FactoryBot.create(:status,user: user)}
+
+  describe 'ステータスを新規作成' do
+    context 'ユーザがステータスを新規作成した場合' do
+      it 'ステータスを作成できる' do
+        visit new_user_session_path
+        fill_in 'user_email', with: 'user10@example.com'
+        fill_in 'user_password', with: 'password'
+        click_button 'ログイン'
+        click_link 'ステータスを作成する'
+        fill_in 'status_name', with: '仕事中'
+        fill_in 'status_description', with: 'wedアプリを作成しています'
+        click_button 'ステータス登録'
+        expect(page).to have_content '仕事中'
+      end
+    end
+  end
+
+  describe 'ステータスを登録' do
+    context 'ユーザがステータスを登録した場合' do
+      it 'ステータスを登録できる' do
+        visit new_user_session_path
+        fill_in 'user_email', with: 'user10@example.com'
+        fill_in 'user_password', with: 'password'
+        click_button 'ログイン'
+        click_link 'ステータスを作成する'
+        fill_in 'status_name', with: '仕事中'
+        fill_in 'status_description', with: 'wedアプリを作成しています'
+        click_button 'ステータス登録'
+        # find('name',text: '休憩中').click
+        click_link'ステータス登録'
+        expect(page).to have_content '休憩中'
+      end
+    end
+  end
+
+
+end
